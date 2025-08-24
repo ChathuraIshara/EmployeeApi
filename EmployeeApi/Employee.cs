3a37eb5ddc7483dfbf229b6a62c6e6602fa2f728
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-08-24 14:08:30 +0530

    Add Employee model

---
+++ EmployeeApi/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeApi
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public required string FirstName { get; set;}  //since we add required,this does not give nullable warning.otherwise we need to add ? in the type to avoid it
+        public required string LastName { get; set; }
+
+    }
+}
