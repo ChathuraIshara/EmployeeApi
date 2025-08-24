ddbf9a883f49f1debd43321e7cd31c4fe5558f11
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-08-24 15:16:19 +0530

    Add route groups

--- EmployeeApi/Program.cs
+++ EmployeeApi/Program.cs
@@ -1,3 +1,5 @@
+using EmployeeApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Employees list
+var employees = new List<Employee>
+{
+    new Employee { id = 1, FirstName = "Chathura", LastName = "Ishara" },
+    new Employee { id = 2, FirstName = "Sam", LastName = "Brown" },
+    new Employee { id = 3, FirstName = "Alice", LastName = "Johnson" }
+};
 var app = builder.Build();
+
+var employeeRoute = app.MapGroup("employees");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,8 +27,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Hello world");
-app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
+
+employeeRoute.MapGet(String.Empty, () => { return Results.Ok(employees); });
+employeeRoute.MapGet("{id:int}", (int id) =>
+{
+    var employee = employees.FirstOrDefault(e => e.id == id);
+    if (employee == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(employee);
+});
+employeeRoute.MapPost(String.Empty, (Employee employee) =>
+{
+    employee.id = employees.Max(e => e.id) + 1; // Assign a new ID
+    employees.Add(employee);
+    return Results.Created($"/employees/{employee.id}", employee);
+
+});
 
 app.UseHttpsRedirection();
 
