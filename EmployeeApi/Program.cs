1cf60cd8f9bd84e8ea000d62ed6ce09f228276d9
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-08-25 21:15:43 +0530

    Add repository pattern

--- EmployeeApi/Program.cs
+++ EmployeeApi/Program.cs
@@ -12,12 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Employees list
-var employees = new List<Employee>
-{
-    new Employee { id = 1, FirstName = "Chathura", LastName = "Ishara", SocialSecurityNumber = "123" },
-    new Employee { id = 2, FirstName = "Sam", LastName = "Brown", SocialSecurityNumber = "456"},
-    new Employee { id = 3, FirstName = "Alice", LastName = "Johnson", SocialSecurityNumber = "879" }
-};
+
 var app = builder.Build();
 
 var employeeRoute = app.MapGroup("employees");
@@ -30,8 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 
-employeeRoute.MapGet(String.Empty, () => { 
-    return Results.Ok(employees.Select(employee => new GetEmployeeResponse
+employeeRoute.MapGet(String.Empty, (EmployeeRepository repository) => { 
+    return Results.Ok(repository.GetAll().Select(employee => new GetEmployeeResponse
     {
         FirstName = employee.FirstName,
         LastName = employee.LastName,
@@ -44,9 +39,9 @@ employeeRoute.MapGet(String.Empty, () => {
         Email = employee.Email
     }));
 });
-employeeRoute.MapGet("{id:int}", ([FromRoute]int id) =>
+employeeRoute.MapGet("{id:int}", ([FromRoute]int id,EmployeeRepository repository) =>
 {
-    var employee = employees.FirstOrDefault(e => e.id == id);
+    var employee = repository.GetById(id);
     if (employee == null)
     {
         return Results.NotFound();
@@ -65,11 +60,10 @@ employeeRoute.MapGet("{id:int}", ([FromRoute]int id) =>
 
     });
 });
-employeeRoute.MapPost(String.Empty, ([FromBody] CreateEmployeeRequest employee) =>
+employeeRoute.MapPost(String.Empty, ([FromBody] CreateEmployeeRequest employee,EmployeeRepository repository) =>
 {
     var newEmployee = new Employee
     {
-        id = employees.Max(e => e.id) + 1, // Generate a new ID
         FirstName = employee.FirstName,
         LastName = employee.LastName,
         SocialSecurityNumber = employee.SocialSecurityNumber,
@@ -81,12 +75,13 @@ employeeRoute.MapPost(String.Empty, ([FromBody] CreateEmployeeRequest employee) 
         PhoneNumber = employee.PhoneNumber,
         Email = employee.Email
     };
+    repository.Create(newEmployee);
     return Results.Created($"/employees/{newEmployee.id}", newEmployee);
 
 });
 
-employeeRoute.MapPut("{id}",([FromBody] UpdateEmployeeRequest updatedEmployee,[FromRoute]int id) =>{
-    var existingEmployee =  employees.FirstOrDefault(e => e.id == id);
+employeeRoute.MapPut("{id}",([FromBody] UpdateEmployeeRequest updatedEmployee,[FromRoute]int id,EmployeeRepository repository) =>{
+    var existingEmployee =  repository.GetById(id);
     if(existingEmployee==null)
     {
         return Results.NotFound();
@@ -98,6 +93,8 @@ employeeRoute.MapPut("{id}",([FromBody] UpdateEmployeeRequest updatedEmployee,[F
     existingEmployee.ZipCode = updatedEmployee.ZipCode;
     existingEmployee.PhoneNumber = updatedEmployee.PhoneNumber;
     existingEmployee.Email = updatedEmployee.Email;
+
+    repository.Update(existingEmployee);
 
     return Results.Ok(existingEmployee);
 });
