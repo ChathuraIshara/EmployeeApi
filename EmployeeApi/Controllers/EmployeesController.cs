52f147c0b4a15cc88fbb548ebca097589d57f7a8
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-09-04 15:02:00 +0530

    Add sepearte endpoints for quering sub resources

--- EmployeeApi/Controllers/EmployeesController.cs
+++ EmployeeApi/Controllers/EmployeesController.cs
@@ -134,6 +134,36 @@ namespace EmployeeApi.Controllers
             };
         }
 
+        /// <summary>
+        /// Gets the benefits for an employee.
+        /// </summary>
+        /// <param name="employeeId">The ID to get the benefits for.</param>
+        /// <returns>The benefits for that employee.</returns>
+        [HttpGet("{employeeId}/benefits")]
+        [ProducesResponseType(typeof(IEnumerable<GetEmployeeResponseEmployeeBenefit>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetBenefitsForEmployee(int employeeId)
+        {
+            var employee = _repository.GetById(employeeId);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee.Benefits.Select(b=>BenefitToBenefitResponse(b)));
+        }
+
+        private static GetEmployeeResponseEmployeeBenefit BenefitToBenefitResponse(EmployeeBenefits benefit)
+        {
+            return new GetEmployeeResponseEmployeeBenefit
+            {
+                Id = benefit.Id,
+                EmployeeId = benefit.EmployeeId,
+                BenefitType = benefit.BenefitType,
+                Cost = benefit.Cost
+            };
+        }
+
 
 
 
